5ad9c1ea2a3112eda56e50e4721a81d92457d3c5
Remington <remingtonwilliams@gmail.com>
2015-07-13 11:25:53 -0500

    do while by 2s

--- while.php
+++ while.php
@@ -2,7 +2,13 @@
 
 $a = 5;
 while ($a <= 15) {
-    echo "\$a is equal to {$a}\n";
+	if ($a % 4 == 0){
+		echo "$a is divisible by 4 and 2\n";
+	}else if ($a % 2 == 0){
+		echo "$a is divisible by 2\n";
+	}
+
+    echo "{$a}\n";
     $a++;
 }
 
