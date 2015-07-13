19f4d4da8a9c85f4ead3c4e8647b4e2c03d84406
Remington <remingtonwilliams@gmail.com>
2015-07-13 10:25:12 -0500

    else if and or

--- if.php
+++ if.php
@@ -45,4 +45,43 @@
  }
 
 
+
+ // Shorten the next 2 statements into an if/else
+ if ($a < $b) {
+     echo "$a is less than $b\n";
+ }else if ($a > $b) {
+     echo "$b is greater than $a\n";
+ }
+
+ // Shorten the next 2 statements into an if/else
+ if ($b >= $c) {
+     // output the appropriate result
+     echo "$b is greater than or equal to $c\n";
+ }else if ($b <= $c) {
+     // output the appropriate result
+     echo "$b is less than or equal to $c\n";
+ }
+
+ // combine the next 4 conditionals into
+ // one if/else/elseif block that checks in order for:
+ // identical, equal, not identical, not equal
+ 
+
+ if ($b === $c) {
+     // output the appropriate result
+     echo "$b is identical to $c\n";
+ }else if ($b == $c) {
+     // output the appropriate result
+     echo "$b is equal to $c\n";
+ }else if ($b !== $c) {
+     // output the appropriate result
+     echo "$b is not identical to $c\n";
+ }else  if ($b != $c) {
+     // output the appropriate result
+     echo "$b is not equal to $c\n";
+ }
+
+ 
+
+
 ?>
