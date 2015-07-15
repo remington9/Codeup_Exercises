97a1727febe8c5c1ce3f979c878545aed8d2d35d
Remington <remingtonwilliams@gmail.com>
2015-07-15 13:39:05 -0500

    functions

--- arithmetic.php
+++ arithmetic.php
@@ -1,30 +1,49 @@
 <?php
 
-function add($a, $b)
-{
-    return $a + $b;
+function add($a, $b){
+    if (is_numeric($a) && is_numeric($b)) {
+        return $a + $b;
+    } else {
+        return "ERROR: Both arguments must be numbers\n";
+    }
 }
 
-function subtract($a, $b)
-{
-	return $a - $b;
+function subtract($a, $b){
+	if (is_numeric($a) && is_numeric($b)) {
+        return $a - $b;
+    } else {
+        return "ERROR: Both arguments a: {$a} and b: {$b} must be numbers\n";
+    }
 }
 
-function multiply($a, $b)
-{
-	return $a * $b;
+function multiply($a, $b){
+	if (is_numeric($a) && is_numeric($b)) {
+        return $a * $b;
+    } else {
+        return "ERROR: Both arguments a: {$a} and b: {$b} must be numbers\n";
+    }
 }
 
-function divide($a, $b)
-{
-    return $a / $b;
+function divide($a, $b){
+    if (is_numeric($a) && is_numeric($b)) {
+    	if($a == 0 || $b == 0){
+    		return "ERROR: Can not divide by 0";
+    	}else{
+        return $a / $b;
+	    }
+    } else {
+        return "ERROR: Both arguments a: {$a} and b: {$b} must be numbers\n";
+    }
 }
-function modulus($a, $b)
-{
-    return $a % $b;
+function modulus($a, $b){
+    if (is_numeric($a) && is_numeric($b)) {
+        return $a % $b;
+    } else {
+        return "ERROR: Both arguments a: {$a} and b: {$b} must be numbers\n";
+    }
 }
 
-$a = 6;
+$a = 'six';
 $b = 3;
 
 // Add code to test your functions here
