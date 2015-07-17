32b6484dc5505b00d63b56df351aaea6c391d9ce
Remington <remingtonwilliams@gmail.com>
2015-07-17 11:06:06 -0500

    lists with commas

--- merge_arrays.php
+++ merge_arrays.php
@@ -14,11 +14,7 @@ function combineArrays($compare, $names){
 	print_r($new). PHP_EOL;
 }
 
-
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
-
-
-
 
 combineArrays($compare, $names);
